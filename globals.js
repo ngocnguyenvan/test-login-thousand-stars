9eae9c2b698dd82411f03622fa94db35eab20542
ngocnguyenvan <ngoc.nguyenvan@powergatevietnam.co>
2016-11-08 14:04:43 +0700

    Add files via upload

---
+++ globals.js
@@ -0,0 +1,12 @@
+const constantsLogin = {
+  emailBlank : '',
+  passwordBlank : '',
+  emailError : 'emailError@gmail.com',
+  passwordError : 'passwordError',
+  emailPass : 'ngoc.nguyenvan@powergatevietnam.com',
+  passwordPass : '123456789Aab'
+};
+export default {
+  waitForConditionTimeout : 4000,
+  constantsLogin : constantsLogin
+};
